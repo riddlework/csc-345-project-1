a79ae1b36a87b3dab5204b79036ee32eabe324e6
nomorepasswords <45520336+nomorepasswords@users.noreply.github.com>
2023-11-21 17:48:54 -0700

    Add files via upload

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 /*
  * Notes Complexity:
- * Space: N = initial solutions d = items = O((700) * d) = O(n)
+ * Space: N = initial # solutions d = items = O(2^(d/2) * d) = O(2^d * d)
  * 
  * Time:
  * n = solutions in population
@@ -10,11 +10,11 @@ import java.util.List;
  * testFitness = O(n*d)
  * mutate pop = O(n*d)
  * testFitness = O(n*d)
- * selection = O(5* (2n) * d)
- * testFitness = O(5 * n*d)
+ * selection = O((d/2)* (2n) * d)
+ * testFitness = O((d/2) * n*d)
  * findBest = O(n)
  * 
- * Total runtime = O(n*d)
+ * Total runtime = O(n*d^2)
  */
 
 
@@ -22,10 +22,10 @@ import java.util.List;
 //The GeneticAlgorithm class is responsible for solving a problem using a genetic algorithm.
 public class GeneticAlgorithm {
 	// The solve method takes a list of items and uses a genetic algorithm to find a solution.
-	public static Solution solve(List<Item> itemList, int threshold) {
+	public static int solve(List<Item> itemList,  List<Item> selectedItems, int threshold) {
 		// Create a population with 20 individuals, each having 8 genes.
-		Population p = new Population(20, threshold);
-		
+		Population p = new Population(1000, threshold);
+		System.out.println("Threshold" + p.weightThreshold);
 		// Initialize the population with random solutions based on the given item list.
 		p.initialPop(itemList);
 		
@@ -40,14 +40,20 @@ public class GeneticAlgorithm {
 		testFitness(p);
 		
 		// Perform selection, crossover, and mutation for a few generations (5 in this case).
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < itemList.size() / 2; i++) {
 			p.selection();
 			testFitness(p);
 		}
 		
 		// Evaluate and display the fitness of the population after selection.
-		return p.findBest();
-		
+		Solution best = p.findBest();
+		System.out.println("This is the best " + best);
+		for(int i = 0; i < itemList.size(); i++) {
+			if(best.solution[i].isExpressed()) {
+				selectedItems.add(itemList.get(i));
+			}
+		}
+		return best.fitness;
 		
 	}
 	
@@ -66,7 +72,7 @@ public class GeneticAlgorithm {
 			}
 			
 			// Update the fitness of the solution if it satisfies the weight constraint.
-			if(weightSum < p.weightThreshold) {
+			if(weightSum <= p.weightThreshold) {
 				s.fitness = sum;
 			}
 		}
